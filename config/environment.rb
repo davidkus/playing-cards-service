6242a44cbea172f78fd27bd5806d65221e780b03
David Kus <david@kus.dk>
2016-02-27 15:36:10 -0500

    Explicitly requiring application_controller

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,7 @@ require 'bundler'
 Bundler.require(:default)
 Bundler.require(Sinatra::Base.environment)
 
+Bundler.require('./app/controllers/application_controller.rb')
+
 # pull in the helpers and controllers
 Dir.glob('./app/{services,controllers}/*.rb').each { |file| require file }
