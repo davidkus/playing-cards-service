8f725fe0f5f9cf55e3c4e5ee6299d5cc13075876
David Kus <david@kus.dk>
2016-03-06 10:18:54 -0500

    Fixing up require

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,5 @@ require 'bundler'
 Bundler.require(:default)
 Bundler.require(Sinatra::Base.environment)
 
-Bundler.require('./app/controllers/application_controller.rb')
-
 # pull in the helpers and controllers
 Dir.glob('./app/{services,controllers}/*.rb').each { |file| require file }
